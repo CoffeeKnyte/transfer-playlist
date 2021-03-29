7603823e115ced767aff5eced02cb4ca876d9a8e
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2021-03-29 23:35:54 +0000

    Create export_pl.sh

---
+++ export_pl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sqplex="sqlite3"
+export_db="com.plexapp.plugins.library.db"
+
+"$sqplex" "$export_db" ".headers off" "SELECT id FROM metadata_items WHERE metadata_type = 15" > export_id.txt
+#echo $export_id
+export_id=($(cat export_id.txt))
+#export_id=1359083
+for j in "${export_id[@]}"; do   
+   foldername=$("$sqplex" "$export_db" "SELECT account_id FROM metadata_item_accounts WHERE metadata_item_id = ${j};")
+   if [ ! -z "$foldername" ]; then
+     mkdir -p "$foldername"
+     filename=$("$sqplex" "$export_db" "SELECT title_sort FROM metadata_items WHERE metadata_type = 15 AND id = ${j};" | sed 's/[^A-Za-z0-9._-]/./g')
+     echo $filename
+     "$sqplex" "$export_db" ".headers on" ".mode line" "SELECT id, guid, media_item_count, title, title_sort, duration, extra_data FROM metadata_items WHERE metadata_type = 15 AND id = ${j};" | sed -e 's/^[ \t]*//' | sed 's/= /\= \"/g' | sed "s/'/''/g" | sed 's/$/\"/g' > "$foldername/$filename.sav"
+     echo "account_id=$foldername" >> "$foldername/$filename.sav"
+   
+     declare -a rating_keys=($("$sqplex" "$export_db" ".headers off" "SELECT metadata_item_id FROM play_queue_generators WHERE playlist_id = ${j}"))
+     #echo ${rating_keys[1]}
+     printf "item_guid=( " >> "$foldername/$filename.sav"
+     for k in "${!rating_keys[@]}"; do item_guid[$k]=$("$sqplex" "$export_db" "SELECT guid FROM metadata_items WHERE id = ${rating_keys[$k]};"); printf "\"${item_guid[$k]}\" "; done >> "$foldername/$filename.sav"
+     printf ")\n" >> "$foldername/$filename.sav"
+     sed -i 's/ = /=/g' "$foldername/$filename.sav"
+   fi
+done
+
+rm export_id.txt
