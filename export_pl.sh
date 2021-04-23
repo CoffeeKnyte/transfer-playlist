618cc30699fa1921836b50d161d8ce2605394a47
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2021-04-23 23:11:28 +0000

    Update export_pl.sh

--- export_pl.sh
+++ export_pl.sh
@@ -10,7 +10,7 @@ for j in "${export_id[@]}"; do
    foldername=$($sqplex "$export_db" "SELECT account_id FROM metadata_item_accounts WHERE metadata_item_id = ${j};")
    if [ ! -z "$foldername" ]; then
      mkdir -p "$foldername"
-     filename=$($sqplex "$export_db" "SELECT title_sort FROM metadata_items WHERE metadata_type = 15 AND id = ${j};" | sed 's/[^A-Za-z0-9._-]/./g')
+     filename=$($sqplex "$export_db" "SELECT title_sort FROM metadata_items WHERE metadata_type = 15 AND id = ${j};" | sed 's/ /./g' | sed 's/[^A-Za-z0-9._-]//g')
      echo $filename
      $sqplex "$export_db" ".headers on" ".mode line" "SELECT id, guid, media_item_count, title, title_sort, duration, extra_data FROM metadata_items WHERE metadata_type = 15 AND id = ${j};" | sed -e 's/^[ \t]*//' | sed 's/= /\= \"/g' | sed "s/'/''/g" | sed 's/$/\"/g' > "$foldername/$filename.sav"
      echo "account_id=$foldername" >> "$foldername/$filename.sav"
@@ -20,21 +20,18 @@ for j in "${export_id[@]}"; do
      printf "item_guid=( " >> "$foldername/$filename.sav"
      for k in "${!rating_keys[@]}"; do item_guid[$k]=$($sqplex "$export_db" "SELECT guid FROM metadata_items WHERE id = ${rating_keys[$k]};"); printf "\"${item_guid[$k]}\" "; done >> "$foldername/$filename.sav"
      printf ")\n" >> "$foldername/$filename.sav"
+     #echo $lib_ids
+     printf "lib_ids=( " >> "$foldername/$filename.sav"
+     for k in "${!rating_keys[@]}"; do 
+		lib_ids[$k]=$($sqplex "$export_db" "SELECT library_section_id FROM metadata_items WHERE id = ${rating_keys[$k]};")
+		#lib_names[$k]=$($sqplex "$export_db" "SELECT name FROM library_sections WHERE id = ${lib_ids[$k]};")
+		printf "\"${lib_ids[$k]}\" "; done >> "$foldername/$filename.sav"
+     printf ")\n" >> "$foldername/$filename.sav"
      sed -i 's/ = /=/g' "$foldername/$filename.sav"
    fi
 done
 
-#remove leading dots to unhide files
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
-find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
+
+#find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
+#find -mindepth 1 -depth -exec rename 's{/\.([^\/]*$)}{/$1}' {} +
 rm export_id.txt
